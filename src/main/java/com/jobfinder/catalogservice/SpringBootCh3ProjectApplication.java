122cb00ef1bec35fb099ea7f969d1ed7b321fdd6
DylanBlevins49 <dylanblevins49@gmail.com>
2025-04-24 16:39:42 -0600

    initial commit

---
+++ src/main/java/com/jobfinder/catalogservice/SpringBootCh3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jobfinder.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCh3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCh3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobfinder/catalogservice/SpringBootCh3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobfinder.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCh3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
